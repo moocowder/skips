08dd365e7169a35bd68850fb90efce3ec400384f
maathi <moowritescode@gmail.com>
2025-06-07 18:25:07 +0100

    fetching skips from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,50 @@
+import { useEffect, useState } from "react"
+import type { SkipType } from "./types/skip"
+
 function App() {
+  const [skips, setSkips] = useState<SkipType[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchSkips = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_API_URL)
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`)
+        }
+        const data: SkipType[] = await response.json()
+        setSkips(data)
+      } catch (err: any) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchSkips()
+  }, [])
+
+  if (loading) return <p>Loading skips...</p>
+  if (error) return <p>Error: {error}</p>
+
   return (
-    <div className="font-semibold caret-teal-500 text-7xl">hello, work!</div>
+    <div>
+      <h2>Available Skips</h2>
+      <ul>
+        {skips.map((skip) => (
+          <li key={skip.id}>
+            <strong>Size:</strong> {skip.size} yards
+            <strong> Hire Days:</strong> {skip.hire_period_days}
+            <strong> Price :</strong> £{skip.price_before_vat}
+            <strong> allowed on road :</strong>
+            {skip.allowed_on_road.toString()}
+            <strong> allows heavy waste :</strong>
+            {skip.allows_heavy_waste.toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
---
+++ src/types/skip.ts
@@ -0,0 +1,8 @@
+export type SkipType = {
+  id: number
+  size: number
+  hire_period_days: number
+  price_before_vat: number
+  allowed_on_road: boolean
+  allows_heavy_waste: boolean
+}
